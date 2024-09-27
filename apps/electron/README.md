257badb8f3c906e6d673be34f4af35ed5134e8b3
Francesco Colamonici <fr-esco@users.noreply.github.com>
2024-09-27 06:34:31 +0200

    chore: 🎉 setup electron

---
+++ apps/electron/README.md
@@ -0,0 +1,7 @@
+# electron
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build electron` to build the library.
---
+++ apps/electron/src/lib/preload.ts
@@ -0,0 +1,24 @@
+// See the Electron documentation for details on how to use preload scripts:
+// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+import { contextBridge, ipcRenderer } from "electron";
+
+console.log("⏪", "Preload???");
+
+contextBridge.exposeInMainWorld("electron", {
+  send: (channel: string, data: any) => {
+    ipcRenderer.send(channel, data);
+  },
+  on: (channel: string, func: (...args: any[]) => void) => {
+    const newFunc = (...args: any[]) => func(...args);
+    ipcRenderer.on(channel, newFunc);
+  },
+  sendSync: (channel: string, data: any) => {
+    return ipcRenderer.sendSync(channel, data);
+  },
+  removeListener: (channel: string, func: (...args: any[]) => void) => {
+    ipcRenderer.removeListener(channel, func);
+  },
+  gcloud: (command: string) => {
+    return ipcRenderer.invoke("gcloud:run-command", command);
+  },
+});
